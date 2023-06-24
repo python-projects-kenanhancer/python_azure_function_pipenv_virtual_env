0dc194f029c949024760c178b7961b8e4255e718
kenan <kenan@10dot1.co.uk>
2023-06-24 13:57:22 +0100

    readme is updated and queue trigger is added

--- function_app.py
+++ function_app.py
@@ -26,3 +26,11 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+# curl --request POST -H "Content-Type: application/json" --data '{"input":"sample queue data"}' http://localhost:7071/admin/functions/QueueFunc
+@app.function_name("QueueFunc")
+@app.queue_trigger(arg_name="azqueue", queue_name="myqueue",
+                               connection="AzureWebJobsStorage") 
+def QueueTrigger(azqueue: func.QueueMessage):
+    logging.info('Python Queue trigger processed a message: %s',
+                azqueue.get_body().decode('utf-8'))
